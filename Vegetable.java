07efdb1389198ea8eceaae8a06299639f378102d
daniel <daniel.097@hotmail.com>
2018-01-05 00:54:44 +0000

    Update Vegetable.java

--- Vegetable.java
+++ Vegetable.java
@@ -1,18 +1,10 @@
-
-/**
- * Abstract class Vegetable - write a description of the class here
- *
- * @author (your name here)
- * @version (version number or date here)
- */
 public abstract class Vegetable implements Food, GridObject 
 {
-    // instance variables - replace the example below with your own
     int calories;
     String name;
     
     
-    public String getName(){ // concrete method
+    public String getName(){
         return name;
     }
 }
